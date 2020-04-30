f7b37b9a62d9398a8110aa4720ec8389943f8530
sariirkh <puspitasarirakhmatullah@gmail.com>
2020-04-30 14:23:06 +0700

    template customer

---
+++ application/views/customer/_template/js.php
@@ -0,0 +1,9 @@
+  <!-- Bootstrap core JavaScript -->
+  <script src="<?php echo base_url('asset-customer/vendor/jquery/jquery.min.js') ?>"></script>
+  <script src="<?php echo base_url('asset-customer/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="<?php echo base_url('asset-customer/vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="<?php echo base_url('asset-customer/js/grayscale.min.js') ?>"></script>
---
+++ application/views/customer/_template/topbar.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top">
+  <!-- Brand -->
+  <a class="navbar-brand" href="#">TrajekLine</a>
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Paket Tour</a>
+    </li>
+
+    <!-- Dropdown -->
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Bantuan
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Profil TrajekLine</a>
+        <a class="dropdown-item" href="#">Ketentuan & Persyaratan</a>
+        <a class="dropdown-item" href="#">Cara Pembayaran</a>
+        <a class="dropdown-item" href="#">Hubungi Kami</a>
+      </div>
+    </li>
+
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Register
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Daftar</a>
+        <a class="dropdown-item" href="#">Login</a>
+      </div>
+    </li>
+  </ul>
+</nav>
+<br>
+
