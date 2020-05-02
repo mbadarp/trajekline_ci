dc7408245783b2dfc013f90a145dd9b529f16a61
mbadarp <58158602+mbadarp@users.noreply.github.com>
2020-05-02 14:53:32 +0700

    pembaruan table saran

--- application/views/crud/data_saran.php
+++ application/views/crud/data_saran.php
@@ -3,40 +3,49 @@
 // $getGrup = $this->session->userdata('session_grup');
 ?>
 <head>
-	<title>Data Admin</title>
+	<title>Data Saran</title>
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
 <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url();?>assets/js/jquery-1.7.1.min.js"></script>
 </head>
 <body>
+          <!-- Begin Page Content -->
+          <div class="container-fluid">
+
+<!-- DataTales Example -->
 <div class="card shadow mb-4">
-    <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">Tabel Data Admin</h6>
-    <br>
-    
-    </div>
-    <div class="card-body">
+  <div class="card-header py-3">
+    <h6 class="m-0 font-weight-bold text-primary datatable">Data Saran</h6>
+   
+  </div>
+  <div class="card-body">
     <div class="table-responsive">
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
         <thead>
-        <tr><th>No</th>
-            <th>ID Saran</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Subjek</th>
-            <th>Saran Masukkan</th>
-            <th>Aksi</th>
-            </tr>
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col">ID Saran</th>
+            <th scope="col">Nama</th>
+            <th scope="col">Email</th>
+            <th scope="col">Subjek</th>
+            <th scope="col">Saran & Kritik</th>
+            <th scope="col">
+              <center><span>Action</span></center>
+            </th>
+          </tr>
         </thead>
         <tfoot>
-        <tr><th>No</th>
-        <th>ID Saran</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Subjek</th>
-            <th>Saran Masukkan</th>
-            <th>Aksi</th>
-        </tr>
+          <tr>
+          <th scope="col">No.</th>
+            <th scope="col">ID Saran</th>
+            <th scope="col">Nama</th>
+            <th scope="col">Email</th>
+            <th scope="col">Subjek</th>
+            <th scope="col">Saran & Kritik</th>
+            <th scope="col">
+              <center><span>Action</span></center>
+            </th>
+          </tr>
         </tfoot>
         <tbody>
         <?php $no =1;
@@ -61,10 +70,10 @@
         </tr>
             <?php }?>
         </tbody>
-        </table>
+      </table>
     </div>
-  
-    
-    </div>
-    </div>
+  </div>
 </div>
+
+</div>
+<!-- /.container-fluid -->
