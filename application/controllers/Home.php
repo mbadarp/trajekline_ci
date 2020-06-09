737435ccb1ad4a042dd074019f27effa89934acd
mbadarp <58158602+mbadarp@users.noreply.github.com>
2020-06-09 12:09:34 +0700

    detail paket

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,9 +9,18 @@ class Home extends CI_Controller{
     public function index (){
         $data['paket']= $this->Home_model->getAll()->result();
         $this->template_customer->views('Home_view', $data);
-       
     }
-    
+    public function detail_paket($id_paket){
+        $where = array('id_paket' => $id_paket);
+       $data['paket'] = $this->Home_model->get_detail($where, 'paket_tour')->result();
+       $this->template_customer->views('paket_detail',$data);
+    //    $this->load->view('paket_detail',$data);
+    }
+    public function edit($id_admin){
+        $where = array('id_admin' => $id_admin);
+        $data['user'] = $this->admin_model->edit_data($where, 'login_admin')->result();
+        $this->template->views('crud/edit_admin',$data);
+    }
 }
 
 ?>
